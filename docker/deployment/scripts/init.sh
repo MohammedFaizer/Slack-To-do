1e83b2e029f92c47909ce60b58c12e05d0f42b71
Mohammed Faizer <zmd5zer@gmail.com>
2024-07-16 12:23:12 +0530

    Docker changes

--- docker/deployment/scripts/init.sh
+++ docker/deployment/scripts/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create a common network for all the services for communication
-APP_NETWORK_NAME=payrentz_backend
+APP_NETWORK_NAME=todoslack_backend
 if [[ "$(docker network ls | grep -ow "${APP_NETWORK_NAME}")" == "" ]]; then
     docker network create "${APP_NETWORK_NAME}"
 fi
